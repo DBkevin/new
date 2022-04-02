64818466fc721a5f865f933bdc4161bec441ef90
dbkevin <416606903@qq.com>
2022-04-02 05:11:10 +0000

    全局模板传参＇ ＇

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,22 +1,22 @@
 <div class="header">
 	<div class="head h-100">
-		<div class="mylike h-100"><a href="/"><img src="https://pccdn.mylike.com/images/mylike.jpg" class="mx-auto mt-10" alt="美莱整形医院"></a></div>
+		<div class="mylike h-100"><a href="/"><img src="{{asset('img/logoHead.jpg')}}" class="mx-auto mt-10" alt="厦门美莱整形医院Logo"></a></div>
 	</div>
 	<div class="bg-baseColor h-50">
 		<ul class="mx-auto h-50 w-1240 text-white leading-50 flex flex-nowrap flex-row items-center justify-between">
-			<li class="flex-1"><a href="/" class="inline text-white" title="美莱品牌连锁">首页</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="厦门美莱品牌连锁">首页</a></li>
 			<li class="flex-1">
 				<a href="/sitemap.html" class="inline text-white" title="美莱整形美容项目">项目</a>
 			</li>
 			<li class="flex-1">
 				<a href="/brand/" class="inline text-white" title="美莱医疗美容(连锁)医院集团">美莱品牌</a>
 			</li>
-			<li class="flex-1"><a href="/doctor/" class="inline text-white" title="美莱整形美容医生">医生</a></li>
-			<li class="flex-1"><a href="/hospital/" class="inline text-white" title="美莱整形美容医院">医院</a></li>
-			<li class="flex-1"><a href="/ask/" class="inline text-white" title="美莱整形美容问答">问答</a></li>
-			<li class="flex-1"><a href="/video/" class="inline text-white" title="美莱整形美容视频">视频</a></li>
-			<li class="flex-1"><a href="/zixun/" class="inline text-white" title="美莱整形美容资讯">资讯</a></li>
-			<li class="flex-1"><a href="/zx/" class="inline text-white" title="美莱整形美容知识">知识</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容医生">医生</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容医院">医院</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容问答">问答</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容视频">视频</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容资讯">资讯</a></li>
+			<li class="flex-1"><a href="/" class="inline text-white" title="美莱整形美容知识">知识</a></li>
 		</ul>
 	</div>
 </div>
--- resources/views/layouts/like.blade.php
+++ resources/views/layouts/like.blade.php
@@ -1,6 +1,8 @@
 <div class="w-full bg-black">
 	<div class="w-1240 text-white h-24 py-3 mx-auto">
 		<p class="text-xl">友情链接</p>
-		<a href="" class="text-sm "><span class="text-gray-500">123</span></a>
+		@foreach($Links as $link)
+		<a href="{{$link->url}}" class="text-sm "><span class="text-gray-500">{{$link->name}}</span></a>
+		@endforeach
 	</div>
 </div>
