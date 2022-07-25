07e8cc3ab6bd3c85e578ba73987f938ed878d0b4
dbkevin <416606903@qq.com>
2022-07-25 06:55:30 +0000

    flx question indexView

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -10,13 +10,14 @@ class QuestionController extends Controller
     //
     public function index(Question $Question)
     {
-        $zs = Question::with(['Topic'])->get()->toArray();
+        $zs = Question::with(['Topic'])->get();
         //内容多了在放出来
         return view('question.index', compact('zs'));
     }
     public function show(Request $request)
     {
         $question = Question::findOrFail($request->id)->load("doctor");
+        //相关根据ID查询暂无,有了再改
         return view('question.show', compact('question'));
     }
 }
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -19,26 +19,29 @@
 				<i class="text-white text-sm inline-block w-8 h-5 aligen-center absolute bg-question-top rounded-lg">精选</i>
 			</h1>
 			<ul>
+				@foreach($zs as $item)
 				<li class="pt-6 pb-4 w-full border-b border-question-border border-solid">
 					<div class="text-lg text-333 w-full truncate">
 						<span style="background: url('/images/question-master.png'); background-position:-102px -34px;" class="inline-block w-7 h-7 mr-4   align-middle"></span>
-						<a href="#">​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？</a>
+						<a href="{{route('questionShow',[$item->id])}}">
+							{{$item->title}}
+						</a>
 					</div>
 					<div class="text-bash text-999 mt-6 flex justify-between">
 						<span class="w-1/10 text-question-inde text-sm block ">
 							最佳回复
 						</span>
 						<p class="w-9/10 overflow-ellipsis overflow-hidden h-auto" style="line-height: 24px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 2;">
-							注射瘦脸并不是持久的，注射瘦脸的主要成分是a型针剂，注射到面部的肌肉以后，随着时间的推移，注射瘦脸可以被人体的新陈代谢所吸收，这时候面部的肌肉和神经会重新取得联系，从而出现反弹的情况，要想达到继续瘦脸的效果，需要不断的注射来维持效果。
+							{{$item->abody}}
 						</p>
 					</div>
 					<div class="text-sm mt-6 text-999 flex justify-between">
 						<span class="inline-flex ">
 							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-56px -35px" class="inlike-block w-4 h-4 mr-2.5 align-sub"></i>
 							<span>本答案由专业 &nbsp;</span>
-							<span class="text-question-inde">张少军</span>
+							<a class="text-question-inde" href="{{route('doctorShow',[$item->Doctor->id])}}">{{$item->Doctor->name}}</a>
 							<span>&nbsp;提供</span>
-							<span class=" ml-3">发布于2022-03-04 15:37:41</span>
+							<span class=" ml-3">发布于{{$item->created_at}}</span>
 						</span>
 						<span class="inline-flex">
 							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-28px -35px" class="inlike-block w-4.5 h-4.5 mr-2 align-sub"></i>
@@ -46,114 +49,7 @@
 						</span>
 					</div>
 				</li>
-				<li class="pt-6 pb-4 w-full border-b border-question-border border-solid">
-					<div class="text-lg text-333 w-full truncate">
-						<span style="background: url('/images/question-master.png'); background-position:-102px -34px;" class="inline-block w-7 h-7 mr-4   align-middle"></span>
-						<a href="#">​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？</a>
-					</div>
-					<div class="text-bash text-999 mt-6 flex justify-between">
-						<span class="w-1/10 text-question-inde text-sm block ">
-							最佳回复
-						</span>
-						<p class="w-9/10 overflow-ellipsis overflow-hidden h-auto" style="line-height: 24px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 2;">
-							注射瘦脸并不是持久的，注射瘦脸的主要成分是a型针剂，注射到面部的肌肉以后，随着时间的推移，注射瘦脸可以被人体的新陈代谢所吸收，这时候面部的肌肉和神经会重新取得联系，从而出现反弹的情况，要想达到继续瘦脸的效果，需要不断的注射来维持效果。
-						</p>
-					</div>
-					<div class="text-sm mt-6 text-999 flex justify-between">
-						<span class="inline-flex ">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-56px -35px" class="inlike-block w-4 h-4 mr-2.5 align-sub"></i>
-							<span>本答案由专业 &nbsp;</span>
-							<span class="text-question-inde">张少军</span>
-							<span>&nbsp;提供</span>
-							<span class=" ml-3">发布于2022-03-04 15:37:41</span>
-						</span>
-						<span class="inline-flex">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-28px -35px" class="inlike-block w-4.5 h-4.5 mr-2 align-sub"></i>
-							0
-						</span>
-					</div>
-				</li>
-				<li class="pt-6 pb-4 w-full border-b border-question-border border-solid">
-					<div class="text-lg text-333 w-full truncate">
-						<span style="background: url('/images/question-master.png'); background-position:-102px -34px;" class="inline-block w-7 h-7 mr-4   align-middle"></span>
-						<a href="#">​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？</a>
-					</div>
-					<div class="text-bash text-999 mt-6 flex justify-between">
-						<span class="w-1/10 text-question-inde text-sm block ">
-							最佳回复
-						</span>
-						<p class="w-9/10 overflow-ellipsis overflow-hidden h-auto" style="line-height: 24px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 2;">
-							注射瘦脸并不是持久的，注射瘦脸的主要成分是a型针剂，注射到面部的肌肉以后，随着时间的推移，注射瘦脸可以被人体的新陈代谢所吸收，这时候面部的肌肉和神经会重新取得联系，从而出现反弹的情况，要想达到继续瘦脸的效果，需要不断的注射来维持效果。
-						</p>
-					</div>
-					<div class="text-sm mt-6 text-999 flex justify-between">
-						<span class="inline-flex ">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-56px -35px" class="inlike-block w-4 h-4 mr-2.5 align-sub"></i>
-							<span>本答案由专业 &nbsp;</span>
-							<span class="text-question-inde">张少军</span>
-							<span>&nbsp;提供</span>
-							<span class=" ml-3">发布于2022-03-04 15:37:41</span>
-						</span>
-						<span class="inline-flex">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-28px -35px" class="inlike-block w-4.5 h-4.5 mr-2 align-sub"></i>
-							0
-						</span>
-					</div>
-				</li>
-				<li class="pt-6 pb-4 w-full border-b border-question-border border-solid">
-					<div class="text-lg text-333 w-full truncate">
-						<span style="background: url('/images/question-master.png'); background-position:-102px -34px;" class="inline-block w-7 h-7 mr-4   align-middle"></span>
-						<a href="#">​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？</a>
-					</div>
-					<div class="text-bash text-999 mt-6 flex justify-between">
-						<span class="w-1/10 text-question-inde text-sm block ">
-							最佳回复
-						</span>
-						<p class="w-9/10 overflow-ellipsis overflow-hidden h-auto" style="line-height: 24px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 2;">
-							注射瘦脸并不是持久的，注射瘦脸的主要成分是a型针剂，注射到面部的肌肉以后，随着时间的推移，注射瘦脸可以被人体的新陈代谢所吸收，这时候面部的肌肉和神经会重新取得联系，从而出现反弹的情况，要想达到继续瘦脸的效果，需要不断的注射来维持效果。
-						</p>
-					</div>
-					<div class="text-sm mt-6 text-999 flex justify-between">
-						<span class="inline-flex ">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-56px -35px" class="inlike-block w-4 h-4 mr-2.5 align-sub"></i>
-							<span>本答案由专业 &nbsp;</span>
-							<span class="text-question-inde">张少军</span>
-							<span>&nbsp;提供</span>
-							<span class=" ml-3">发布于2022-03-04 15:37:41</span>
-						</span>
-						<span class="inline-flex">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-28px -35px" class="inlike-block w-4.5 h-4.5 mr-2 align-sub"></i>
-							0
-						</span>
-					</div>
-				</li>
-				<li class="pt-6 pb-4 w-full border-b border-question-border border-solid">
-					<div class="text-lg text-333 w-full truncate">
-						<span style="background: url('/images/question-master.png'); background-position:-102px -34px;" class="inline-block w-7 h-7 mr-4   align-middle"></span>
-						<a href="#">​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？​自体脂肪丰全脸有什么副作用吗？</a>
-					</div>
-					<div class="text-bash text-999 mt-6 flex justify-between">
-						<span class="w-1/10 text-question-inde text-sm block ">
-							最佳回复
-						</span>
-						<p class="w-9/10 overflow-ellipsis overflow-hidden h-auto" style="line-height: 24px;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 2;">
-							注射瘦脸并不是持久的，注射瘦脸的主要成分是a型针剂，注射到面部的肌肉以后，随着时间的推移，注射瘦脸可以被人体的新陈代谢所吸收，这时候面部的肌肉和神经会重新取得联系，从而出现反弹的情况，要想达到继续瘦脸的效果，需要不断的注射来维持效果。
-						</p>
-					</div>
-					<div class="text-sm mt-6 text-999 flex justify-between">
-						<span class="inline-flex ">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-56px -35px" class="inlike-block w-4 h-4 mr-2.5 align-sub"></i>
-							<span>本答案由专业 &nbsp;</span>
-							<span class="text-question-inde">张少军</span>
-							<span>&nbsp;提供</span>
-							<span class=" ml-3">发布于2022-03-04 15:37:41</span>
-						</span>
-						<span class="inline-flex">
-							<i style="background-image:url('images/question-master.png');background-size:200px 200px;background-position:-28px -35px" class="inlike-block w-4.5 h-4.5 mr-2 align-sub"></i>
-							0
-						</span>
-					</div>
-				</li>
+				@endforeach
 			</ul>
 		</li>
 		<li class="w-tcright">
