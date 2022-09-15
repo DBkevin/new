2a3fe1142cad1f20299c55ac97fab65bd14c1104
dbkevin <416606903@qq.com>
2022-09-15 09:44:07 +0000

    医生excel 导入功能完事,并跑完

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Doctor;
 use Illuminate\Http\Request;
 use App\Services\IsMobile;
+use App\Models\Topic;
 
 class DoctorController extends Controller
 {
@@ -26,5 +27,37 @@ class DoctorController extends Controller
             return view('Mobies.doctor.show', compact('doctor'));
         }
         return view('doctor.show', compact('doctor'));
+    }
+    /**
+     * 需要加一条路由,先注释
+     * 
+     */
+    public function addDoctor() 
+    {
+        $file = fopen(base_path('./123.txt'), "r");
+        while (!feof($file)) {
+            $a = trim(fgets($file));
+            //转数组
+            $arr = explode(",", $a); //转为数组
+            $tagsArr = array_slice($arr, 6); //截取数组,
+            array_filter($tagsArr); //数组去空
+            $tagsID = Topic::whereIn('title', $tagsArr)->pluck('id')->toArray(); //对应
+            $doctor = new Doctor();
+            $picture = "doctor/" . $arr[1] . ".jpg";
+            $doctor->name = $arr[0];
+            $doctor->picture = $picture;
+            $doctor->title = $arr[2];
+            $doctor->age = $arr[3];
+            $doctor->introduce = $arr[4];
+            $doctor->school = $arr[5];
+            $doctor->save();
+            $doctor->tags()->attach($tagsID);
+            $a=null;
+            $tagsArr=null;
+            $tagsID=null;
+            $doctor=NULL;
+            $picture=null;
+
+        }
     }
 }
